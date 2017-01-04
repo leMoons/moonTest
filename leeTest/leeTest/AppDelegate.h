efd987e96ab920420ef67e67a362ecff87d14880
lee <272436311@qq.com>
2017-01-04 10:03:20 +0800

    项目上传

---
+++ leeTest/leeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  leeTest
+//
+//  Created by 汪宁 on 17/1/3.
+//  Copyright © 2017年 ZHENAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ leeTest/leeTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  leeTest
+//
+//  Created by 汪宁 on 17/1/3.
+//  Copyright © 2017年 ZHENAI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
